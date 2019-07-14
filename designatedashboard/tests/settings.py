858de8f7037d18a956344565cdcfc386f3c01315
Erik Olof Gunnar Andersson <eandersson@blizzard.com>
2019-07-13 22:57:04 -0700

    Fix broken tests
    This fixes the tests by reuseing the more modern approach for
    handling settings from the heat_dashboard.
    
    Finally we remove the base designate jobs, as they do not
    make use of the dashboard.
    
    Change-Id: I53fd7bd0d01680c0bd59ad19365ceede0125050a

--- designatedashboard/tests/settings.py
+++ designatedashboard/tests/settings.py
@@ -11,68 +11,30 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-import socket
 
-SECRET_KEY = 'HELLA_SECRET!'
+# Default to Horizons test settings to avoid any missing keys
 
-DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3',
-                         'NAME': 'test'}}
+import openstack_dashboard.enabled  # noqa: F811
+from openstack_dashboard.test.settings import *  # noqa: F403,H303
+from openstack_dashboard.utils import settings
 
-from horizon.test.settings import *  # noqa
+import designatedashboard.enabled
 
-socket.setdefaulttimeout(1)
+# pop these keys to avoid log warnings about deprecation
+# update_dashboards will populate them anyway
+HORIZON_CONFIG.pop('dashboards', None)
+HORIZON_CONFIG.pop('default_dashboard', None)
 
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
+# Update the dashboards with designatedashboard enabled files
+# and current INSTALLED_APPS
+settings.update_dashboards(
+    [
+        openstack_dashboard.enabled,
+        designatedashboard.enabled,
+    ],
+    HORIZON_CONFIG,
+    INSTALLED_APPS
+)
 
-TESTSERVER = 'http://testserver'
-
-
-MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
-
-EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
-SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
-
-OPENSTACK_ADDRESS = "localhost"
-OPENSTACK_ADMIN_TOKEN = "openstack"
-OPENSTACK_KEYSTONE_URL = "http://%s:5000/v2.0" % OPENSTACK_ADDRESS
-OPENSTACK_KEYSTONE_ADMIN_URL = "http://%s:35357/v2.0" % OPENSTACK_ADDRESS
-OPENSTACK_KEYSTONE_DEFAULT_ROLE = "Member"
-
-# Silence logging output during tests.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'logging.NullHandler'
-        },
-    },
-    'loggers': {
-        'django.db.backends': {
-            'handlers': ['null'],
-            'propagate': False,
-        },
-        'horizon': {
-            'handlers': ['null'],
-            'propagate': False,
-        },
-        'novaclient': {
-            'handlers': ['null'],
-            'propagate': False,
-        },
-        'keystoneclient': {
-            'handlers': ['null'],
-            'propagate': False,
-        },
-        'quantum': {
-            'handlers': ['null'],
-            'propagate': False,
-        },
-        'nose.plugins.manager': {
-            'handlers': ['null'],
-            'propagate': False,
-        }
-    }
-}
+# Remove duplicated apps
+INSTALLED_APPS = list(set(INSTALLED_APPS))
