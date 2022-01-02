7de390b31411b33c14ca9ea383eb5f08eca095c2
Takashi Kajinami <tkajinam@redhat.com>
2022-01-02 21:51:37 +0900

    Add example dashboard config file
    This change introduces an example dashboard config file so that users
    can more easily set up the recommended parameters and understand
    the available parameters. Horizon supports loading multiple config
    files from local_settings.d and it is now encouraged to implement
    per-dashboard config file.
    
    Change-Id: I423d4811ead2c0c8f79fc0d80f3c7fe8d3578c97

---
+++ designatedashboard/local_settings.d/_1799_dns_settings.py
@@ -0,0 +1,32 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# This file is to be included for configuring application which relates
+# to dns(Designate) functions.
+from django.conf import settings
+
+settings.POLICY_FILES.update({
+    'dns': 'designate_policy.yaml'
+})
+
+settings.DEFAULT_POLICY_FILES.update({
+    'dns': 'default_policies/designate.yaml'
+})
+
+# Sample
+# settings.LOGGING['loggers'].update({
+#     'designateclient': {
+#         'handlers': ['console'],
+#         'level': 'DEBUG',
+#         'propagate': False,
+#     }
+# })
