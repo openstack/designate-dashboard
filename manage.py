ef686c490cb6ed4df2eca3aad36d1d07574b69d1
Michael Johnson <johnsomor@gmail.com>
2022-06-15 20:02:03 +0000

    Fix manage.py for the default case
    The manage.py included in designate-dashboard pointed to a settings file that does not exist. The tests (in tox.ini) already override these settings to use those under designatedashboard/tests/settings.py.
    This patch updates manage.py to use the settings from horizon by default to reduce the confusion over which manage.py should be used to update the static files.
    
    Change-Id: If2e0efb4240a72bedfbd09988707d4dabe9d5d1f

--- manage.py
+++ manage.py
@@ -18,6 +18,6 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", "designatedashboard.settings")
+        "DJANGO_SETTINGS_MODULE", "openstack_dashboard.settings")
     from django.core.management import execute_from_command_line  # noqa
     execute_from_command_line(sys.argv)
