2d45e0ce048f59f705c3b07f4fe1bf42b1ce5913
Ivan Anfimov <lazekteam@gmail.com>
2025-07-29 09:36:50 +0000

    Ability to translate the group name into other languages
    Change-Id: I1d9a618c65b8734e03143a1af97edeb2449dafa0
    Signed-off-by: Ivan Anfimov <lazekteam@gmail.com>

--- designatedashboard/enabled/_1710_project_dns_panel_group.py
+++ designatedashboard/enabled/_1710_project_dns_panel_group.py
@@ -12,9 +12,11 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from django.utils.translation import gettext_lazy as _
+
 # The name of the panel group to be added to HORIZON_CONFIG. Required.
 PANEL_GROUP = 'dns'
 # The display name of the PANEL_GROUP. Required.
-PANEL_GROUP_NAME = 'DNS'
+PANEL_GROUP_NAME = _('DNS')
 # The name of the dashboard the PANEL_GROUP associated with. Required.
 PANEL_GROUP_DASHBOARD = 'project'
